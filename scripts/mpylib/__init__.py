8a8cf45a17f23db53831f5a7c750f2b2ed591ba2
Eric Vignola <eric.vignola@gmail.com>
2019-02-12 20:03:14 -0800

    removed removed modules from __all__

--- scripts/mpylib/__init__.py
+++ scripts/mpylib/__init__.py
@@ -1,4 +1,4 @@
 from _base.mnode import MNode, MNodeList
 from _base.mundo import MUndo
 
-__all__ = ["MNode", "MShape", "MTrans", "MNodeList", "MShapeList", "MTransList", "MUndo"]
+__all__ = ["MNode", "MUndo"]
